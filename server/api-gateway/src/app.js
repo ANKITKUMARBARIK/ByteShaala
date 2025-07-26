28ee3dc27e019129c8be23148f8b1d4e9215ba16
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-07-26 10:47:00 +0530

    fix: update CORS configuration to use environment variable for frontend URL

--- server/api-gateway/src/app.js
+++ server/api-gateway/src/app.js
@@ -9,52 +9,57 @@ const app = express();
 
 // CORS configuration
 app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie", "x-user-data"],
-  })
+    cors({
+        origin: [process.env.FRONTEND_BASE_URL, "http://127.0.0.1:5173"],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cookie",
+            "x-user-data",
+        ],
+    })
 );
 
 app.use(cookieParser());
 
 // auth service
 app.use(
-  "/api/v1/auth",
-  proxy(process.env.AUTH_SERVICE, {
-    proxyReqPathResolver: (req) => {
-      return `/api/v1/auth${req.url}`;
-    },
-    proxyErrorHandler: (err, res, next) => {
-      console.error("Auth Proxy Error:", err.message);
-      next(err);
-    },
-  })
+    "/api/v1/auth",
+    proxy(process.env.AUTH_SERVICE, {
+        proxyReqPathResolver: (req) => {
+            return `/api/v1/auth${req.url}`;
+        },
+        proxyErrorHandler: (err, res, next) => {
+            console.error("Auth Proxy Error:", err.message);
+            next(err);
+        },
+    })
 );
 
 // user service
 app.use(
-  "/api/v1/user",
-  verifyAuthentication,
-  proxy(process.env.USER_SERVICE, {
-    proxyReqPathResolver: (req) => {
-      return `/api/v1/user${req.url}`;
-    },
-    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-      const user = srcReq.user;
-      if (user) {
-        proxyReqOpts.headers["x-user-data"] = encodeURIComponent(
-          JSON.stringify(user)
-        );
-      }
-      return proxyReqOpts;
-    },
-    proxyErrorHandler: (err, res, next) => {
-      console.error("User Proxy Error:", err.message);
-      next(err);
-    },
-  })
+    "/api/v1/user",
+    verifyAuthentication,
+    proxy(process.env.USER_SERVICE, {
+        proxyReqPathResolver: (req) => {
+            return `/api/v1/user${req.url}`;
+        },
+        proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+            const user = srcReq.user;
+            if (user) {
+                proxyReqOpts.headers["x-user-data"] = encodeURIComponent(
+                    JSON.stringify(user)
+                );
+            }
+            return proxyReqOpts;
+        },
+        proxyErrorHandler: (err, res, next) => {
+            console.error("User Proxy Error:", err.message);
+            next(err);
+        },
+    })
 );
 
 // course service
